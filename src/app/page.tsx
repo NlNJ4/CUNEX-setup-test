0b451e85d5e84f7ad789dbcb636ff572009b4d46
Aluminium51 <noptanadon.p@gmail.com>
2025-10-16 18:00:34 +0700

    feat(setup): Add TextFeild component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Button from "@components/Button";
+import TextField from "@components/TextField";
 export default function Home() {
   return (
-    <div className="bg-white min-h-screen space-y-2"> 
+    <div className="bg-white min-h-screen space-y-2 "> 
       <Button mode="filled" bordered="round" expanded={false}>
         Label
       </Button>
@@ -20,6 +21,10 @@ export default function Home() {
       <Button mode="outline" bordered="round" expanded={true}>
         Label
       </Button>
+      <TextField placeholder="type somthing..."/>
+      <TextField supportingText="supporting text ..." placeholder="type somthing..."/>
+      <TextField error supportingText="error ..." placeholder="type somthing..."/>
+      <TextField endIcon={<span>icon</span>} placeholder="type somthing..."/>
     </div>
   );
 }
